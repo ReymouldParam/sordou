482491eb59a836405f356a44c3203cd2a2411c22
Naresh Narnapati <naresh.narnapati@reymould.com>
2026-03-31 18:40:54 +0530

    footer part email updated

--- footer.php
+++ footer.php
@@ -1,12 +1,10 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $name = $_POST['name'];
     $email = $_POST['email'];
-    $message = $_POST['message'];
 
     $to = "reymould.social@gmail.com";
     $subject = "Enquiry from aquasite  website";
-    $body = "Name: $name\nEmail: $email\nMessage: $message";
+    $body = "Email: $email";
 
     // Send email to both recipients
     mail("pravallika.pentakota@reymould.com", $subject, $body);
